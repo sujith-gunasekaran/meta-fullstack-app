f730a89136d747dcf4b6bcdc2fbb9d03c30bbf33
sujith-gunasekaran <sujith.gunasekaran@freshworks.com>
2024-03-26 19:25:16 +0530

    added multiple interface

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
     window.client.interface
 		.trigger("showModal", {
 			title: "Sample Modal",
-			template: "interface/index.html",
+			template: "interface/modal.html",
 		})
 		.then(function (data) {
 			console.info("modal displayed");
@@ -26,7 +26,22 @@ const Home = () => {
 			console.error("Unable to display modal");
 			console.error(error);
 		});
+  }
 
+  const openDialog = () => {
+    window.client.interface
+		.trigger("showDialog", {
+			title: "Sample Dialog",
+			template: "interface/dialog.html",
+		})
+		.then(function (data) {
+			console.info("dialog displayed");
+			console.info(data);
+		})
+		.catch(function (error) {
+			console.error("Unable to display dialog");
+			console.error(error);
+		});
   }
 
   React.useEffect(() => {
@@ -37,6 +52,7 @@ const Home = () => {
     <React.Fragment>
       <div>Home Component - 1</div>
       <button onClick={() => openModal()}>Open interface</button>
+      <button onClick={() => openDialog()}>Open Dialog</button>
     </React.Fragment>
   )
 }
